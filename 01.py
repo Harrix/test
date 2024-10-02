7b0a37c30d485abb66cc2d72d5ea060b03875964
Sergienko Anton <sergienkoanton@mail.ru>
2024-10-02 15:59:07 +0300

    Update 01.py

--- 01.py
+++ 01.py
@@ -2,8 +2,12 @@ import itertools
 
 for x, y, z, w in itertools.product([0, 1], repeat=4):
     t = int(((x <= y) and (z == (not w)))<=(x == (x or z)))
-    print(x, y, z, w, t)
-print()
+    print(x, y, z, t, w)
+
+
+
 for x, y, z in itertools.product([0, 1], repeat=3):
     t = int(z and (y == (not x)))
     print(x, y, z, t)
+
+ААААААААААААААААААААААААааааааааааааааааааааааааааааааааааааааааааааааааааааааа!!!
